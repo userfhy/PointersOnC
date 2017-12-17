10b0bb1c2c70e70dd02c6449bf39494e64c8e0f9
fanhengyuan <fanhengyuan1994@gmail.com>
2017-12-17 19:00:02 +0800

    p6.c

--- ch06/p6.c
+++ ch06/p6.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define MAX_VALUE 1000
 
@@ -8,16 +8,16 @@ int main(int argc, char const *argv[]) {
   int bit_number;
   int number;
 
-  for( number = 3, bit_number = 0; number <= MAX_VALUE; number += 2, bit_number += 1 ) {
+  for(number = 3, bit_number = 0; number <= MAX_VALUE; number += 2, bit_number += 1) {
     if( number > limits ) {
-      printf("%d-%d:%d\n", limits-1000, limits, n_primes );
+      printf("%d-%d:%d\n", limits-1000, limits, n_primes);
       n_primes = 0;
       limits += 1000;
     }
-    if ( test_bit( sieve, bit_number )) {
+    if ( test_bit(sieve, bit_number) ) {
       n_primes++;
     }
   }
-  printf("%d-%d:%d\n", limits-1000, limits, n_primes );
+  printf("%d-%d:%d\n", limits-1000, limits, n_primes);
   return 0;
 }
