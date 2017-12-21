f9bee4974a7371cf20bd0094d72fc05870f82f83
Fan Hengyuan <fanhengyuan1994@gmail.com>
2017-12-21 13:23:18 +0800

    ch07

---
+++ ch07/p5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#define NUL '\0'
+
+void print_int(int n)
+{
+    printf("%d\n", n);
+}
+
+void print_float(double n)
+{
+    printf("%lf\n", n);
+}
+
+int my_printf(char *format, ...)
+{
+    int num = 0;
+    char ch;
+    char *str;
+    va_list var_list;
+
+    va_start(var_list, format);
+
+    while( (ch = *format++) != NUL )
+    {
+        if( ch != '%' ) {
+            putchar(ch);
+            continue;
+        }
+
+        switch(*format != NUL ? *format++ : NUL) {
+            case 'd':
+                print_int(va_arg(var_list, int));
+                break;
+            case 'f':
+                print_float(va_arg(var_list, double));
+                break;
+            case 's':
+                str = va_arg(var_list, char *);
+                while(*str != NUL) {
+                    putchar(*str++);
+                }
+                break;
+            case 'c':
+                num--;
+                break;
+        }
+        num++;
+    }
+    va_end(var_list);
+    return num;
+}
+
+int main(int argc, char const *argv[])
+{
+    int d = 14;
+    printf("%d\n", my_printf("%d===%f sdfds %f%e%f", d, 3.34, 4.55555, 67868, 89.99) );
+    return 0;
+}
